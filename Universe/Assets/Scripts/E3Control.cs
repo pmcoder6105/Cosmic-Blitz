e68d3dba42f9a5ac133a9a7ed0b1b00cfc4d7d93
Pratyush <pratyush.mahadevaiah@gmail.com>
2021-12-04 17:20:38 -0800

    Worked on Enemy Ship 3 lazers. Not finished yet

---
+++ Universe/Assets/Scripts/E3Control.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E3Control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > 4)
+        {
+            Debug.Log("Test");
+        }
+    }
+}
