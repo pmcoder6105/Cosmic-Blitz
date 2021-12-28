4d289e0455f3b1c5a27d06dd9957ee612999262c
Pratyush <pratyush.mahadevaiah@gmail.com>
2021-12-28 11:42:26 -0800

    Finished most of level  6
    Notes:
    
    MAYBE add more enemies
    ADD MORE DECOR
    Do boss cube
    Also added travel to planet scene

---
+++ Universe/Assets/Scripts/TravelToPlanet.cs
@@ -0,0 +1,20 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TravelToPlanet : MonoBehaviour
+{
+    float verticalControlSpeed = 50;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Translate(0, 0, .2f * Time.deltaTime * verticalControlSpeed);
+        if (Time.timeSinceLevelLoad > 20)
+        {
+            SceneManager.LoadScene(6);
+        }
+    }
+}
