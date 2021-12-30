3729d9e337d0ec67d139329dd9a9350995f13e93
Pratyush <pratyush.mahadevaiah@gmail.com>
2021-12-29 20:44:03 -0800

    Added level selection button
    You can now access any level you want with this feature. Pretty good so far.
    
    NOTES:
    
    Make a Credits menu and a quit button
    
    ALSO:
    When any level starts, create a tooltip panel about the level.
    Hint: use Time.scaleTime

--- Universe/Assets/Scripts/MenuButtons.cs
+++ Universe/Assets/Scripts/MenuButtons.cs
@@ -7,4 +7,34 @@ public class MenuButtons : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void PlayFirstLevelOnLevelSelect()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlaySecondLevelOnLevelSelect()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void PlayThirdLevelOnLevelSelect()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void PlayFourthLevelOnLevelSelect()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void PlayFifthLevelOnLevelSelect()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void PlaySixthLevelOnLevelSelect()
+    {
+        SceneManager.LoadScene(7);
+    }
 }
