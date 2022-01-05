b5b0cfdf360901510dd9578f262d05d3a340e65c
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-01-05 10:00:25 -0800

    Added main audio source
    That is Energy - NCS Elektronomia
    
    Also need to add lazer audio, explosion audio, thrust audio, and I guess that's it for audio.

---
+++ Universe/Assets/Scripts/BGSoundScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+public class BGSoundScript : MonoBehaviour
+{
+    private static BGSoundScript instance = null;
+    public static BGSoundScript Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
