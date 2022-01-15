d164c9103d644548ac7a3fb2698cb176d7c64522
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-01-14 21:10:08 -0800

    Commented on TravelToPlanet.cs
    Scripts left to comment:
    
    MenuButtons.cs
    Mine.cs
    PauseButton.cs

--- Cosmic-Blitz/Assets/Scripts/TravelToPlanet.cs
+++ Cosmic-Blitz/Assets/Scripts/TravelToPlanet.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 
 public class TravelToPlanet : MonoBehaviour
 {
-    [SerializeField] float verticalControlSpeed = 50;
+    [Tooltip("Amount to translate when traveling")][SerializeField] float verticalControlSpeed = 50;
 
-
+    //Set Time to 0 && Set player component to false
     void Start()
     {
         Time.timeScale = 0;
         GetComponent<Player>().enabled = false;
     }
 
+    //Translate forward at a frame rate independant rate and after 10 secs, load the next scene, which is scene #7
     void Update()
     {
         transform.Translate(0, 0, .2f * Time.deltaTime * verticalControlSpeed);
