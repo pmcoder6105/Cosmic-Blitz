7e182f616d3420fd1432979137e305a021dbdb08
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-01-14 22:10:27 -0800

    Commented PauseButton.cs

--- Cosmic-Blitz/Assets/Scripts/PauseButton.cs
+++ Cosmic-Blitz/Assets/Scripts/PauseButton.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 
 public class PauseButton : MonoBehaviour
 {
+    //This method is for turning time to 0 when you click a certain button
     public void PauseOnClick()
     {
         Time.timeScale = 0;
     }
 
+    //This method is for turning time to 1 when you click a certain button
     public void ResumeOnClick()
     {
         Time.timeScale = 1;
     }
 
+    //This method is for going to the first scene, aka the menu, when you click a certain button
     public void ReturnToMenuOnClick()
     {
         SceneManager.LoadScene(0);
