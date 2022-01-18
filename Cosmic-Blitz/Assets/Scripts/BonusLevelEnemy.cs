0daa3c7a1b6f0be5eb59d42787d846bd5d93d8e2
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-01-17 17:53:13 -0800

    Made it so that the ships are constantly moving
    Good stuff so far. Carry on.

---
+++ Cosmic-Blitz/Assets/Scripts/BonusLevelEnemy.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusLevelEnemy : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 4;
+    
+    [Header("All the visuals for the enemy ship")]
+    [Tooltip("This is the explosion")] [SerializeField] ParticleSystem explosion;
+    [Tooltip("This is the explosion vfx for the player ship when the enemy crosses into the home planet")] [SerializeField] ParticleSystem explosionPlayer;
+    [Tooltip("This is the spark that goes off when it takes damage")] [SerializeField] ParticleSystem hitSpark;
+    [Tooltip("This is the enemy thruster")] [SerializeField] ParticleSystem thruster;
+    
+    
+    [Header("All of the references")]
+    [Tooltip("This is the player reference")] Player player;
+    [Tooltip("This is the audiosource reference")] AudioSource aS;
+
+
+    [Tooltip("This is the destruction sfx")] [SerializeField] AudioClip destruction;
+
+    //Make sure to cache our references
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        aS = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        transform.Translate(0, 0, 0.1f * Time.deltaTime * 10);
+        if (hitSpark == null)
+        {
+            return;
+        }
+        if (explosionPlayer == null)
+        {
+            return;
+        }
+    }
+
+    //On particle collision
+    void OnParticleCollision(GameObject other)
+    {
+        //If the particle was a player particle
+        //reduce hit points by 1
+        //play a vfx
+        if (other.gameObject.tag == "Player")
+        {
+            hitPoints = hitPoints - 1;
+            hitSpark.Play();            
+            if (hitPoints == 0)
+            {
+                if (!aS.isPlaying)
+                {
+                    aS.PlayOneShot(destruction);
+                }
+                GetComponent<MeshRenderer>().enabled = false;
+                GetComponent<Enemy>().enabled = false;
+                GetComponent<BoxCollider>().enabled = false;
+                GetComponent<Animator>().enabled = false;
+                if (!explosion.isPlaying)
+                {
+                    explosion.Play();
+                }
+                Invoke(nameof(DestroyWhenDestroyed), 2f);
+                if (thruster != null)
+                {
+                    thruster.Stop();
+                }
+            }
+        }
+    }
+
+    //What happens on collision
+    void OnCollisionEnter(Collision other)
+    {
+        //If the collider is the enemy finish pad
+        //play explosionPlayer vfx
+        //Call DestroyWhenEnemyFinishes from the player script
+        //Finally, load the current scene in 2 seconds
+        if (other.gameObject.tag == "EnemyFinishPad")
+        {                        
+            explosionPlayer.Play();            
+            player.DestroyWhenEnemyFinishes();
+            Invoke(nameof(EnemyLoadCurrentScene), 2f);
+        }
+    }
+    
+    //Load Current Scene
+    void EnemyLoadCurrentScene()
+    {
+        player.LoadCurrentScene();
+    }
+    
+    //Destroy the gameObject
+    void DestroyWhenDestroyed()
+    {
+        Destroy(gameObject);
+    }
+}
